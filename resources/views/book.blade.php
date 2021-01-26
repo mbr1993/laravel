b915557a8e8185ece3928e36c7b8a30076c5825e
Bobur <boburmannonov93@gmail.com>
2021-01-26 21:00:53 +0500

    test

---
+++ resources/views/book.blade.php
@@ -0,0 +1,17 @@
+<h1>Salom</h1>
+
+<?= 'Book'.time()?>
+
+<?php 
+
+    if(true){
+        echo 'true';
+    }else{
+        echo 'false';
+    }
+
+for($i = 1; $i<10; $i++){
+    echo $i.'<br>';
+}
+
+?>
